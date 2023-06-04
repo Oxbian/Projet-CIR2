7cb32e84dc916d556992df9c7b74a6d8ce6658d2
Pier <pier.chapon@orange.fr>
2023-06-04 17:34:17 +0200

    ajout header/footer/box

---
+++ assets/js/box.js
@@ -0,0 +1,59 @@
+const boxes = document.querySelectorAll(".box");
+var container = document.getElementById("liste-morceau1");
+
+container.addEventListener("wheel", checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 10) * 6;
+  boxes.forEach((box, index) => {
+    box.addEventListener("click", function () {
+      box.classList.add("go");
+
+      boxes.forEach((otherBox, otherIndex) => {
+        // Vérifie si la boîte est différente de celle cliquée
+        if (otherBox == box) {
+          otherBox.classList.remove("go2");
+          otherBox.classList.remove("go3");
+        }
+        if (otherBox !== box) {
+          // Supprime la classe "go" et "go2" des autres boîtes
+          otherBox.classList.remove("go");
+          otherBox.classList.remove("go2");
+          otherBox.classList.remove("go3");
+        }
+        if (otherIndex === index - 1) {
+          otherBox.classList.add("go2");
+          otherBox.classList.remove("go3");
+        }
+
+        // Vérifie la boîte en dessous
+        if (otherIndex === index + 1) {
+          otherBox.classList.add("go2");
+          otherBox.classList.remove("go3");
+        }
+
+        if (otherIndex === index - 2) {
+          otherBox.classList.add("go3");
+          otherBox.classList.remove("go2");
+        }
+
+        // Vérifie la boîte en dessous
+        if (otherIndex === index + 2) {
+          otherBox.classList.add("go3");
+          otherBox.classList.remove("go2");
+        }
+      });
+    });
+    const boxTop = box.getBoundingClientRect().top;
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
+function clickbutton() {
+  console.log("yo");
+}
