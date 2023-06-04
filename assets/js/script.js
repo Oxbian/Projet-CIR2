77280c6c295342f4fed8e7b2286cd2d86dc03343
Pier <pier.chapon@orange.fr>
2023-06-04 13:43:04 +0200

    header/footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 var home = document.getElementById("home");
+var search = document.getElementById("rechercheIcon");
 
 home.addEventListener("click", function () {
   window.location.href = "accueil.html";
+});
+
+search.addEventListener("click", function () {
+  window.location.href = "rechercheAlbum.html";
 });
