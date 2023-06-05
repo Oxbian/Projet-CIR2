201235ceb842faf3185bade3b58fc091792782c2
Oxbian <admin.soiree@simplelogin.com>
2023-06-05 13:38:41 +0200

    Ajout du menu de musique écoutée

--- assets/js/fonctionnel/listened.js
+++ assets/js/fonctionnel/listened.js
@@ -1,13 +1,24 @@
 /**
- * Fonction pour charger les informations de l'album
+ * Fonction pour afficher le nom de l'artiste
+ * @param {*} data Informations de l'artiste
+ */
+function loadArtistInfo(data) {
+  const artiste = document.getElementById('album-info');
+  if (artiste) {
+    artiste.innerHTML = `<p>Artiste : ${data.nom} ${data.prenom}</p>`;
+  }
+}
+
+/**
+ * Fonction pour afficher le nom de l'album
  * @param {*} data Informations de l'album
  */
 function loadAlbumInfo(data) {
-  const albumInfo = document.getElementById('album-info');
   const albumNom = document.getElementById('album-nom');
   if (albumNom) {
     albumNom.innerHTML = `<p>Album : ${data.titre}</p>`;
   }
+  ajaxRequest('GET', `../php/request.php/artist/${data.id_artiste}`, loadArtistInfo);
 }
 
 /**
@@ -21,17 +32,17 @@ function loadListenedMusic(data) {
     listeMorceau.innerHTML = '';
     for (let index = 0; index < data.length; index += 1) {
       if (index === 0) {
-        listeMorceau.innerHTML += `<div class="box show"><h2>${data[index].titre}</h2></div>`;
+        listeMorceau.innerHTML += `<div class="box show"><h2>${data[index].titre}</h2><span>${data[index].duree}</span></div>`;
         ajaxRequest('GET', `../php/request.php/album/${data[index].id_album}`, loadAlbumInfo);
       } else {
-        listeMorceau.innerHTML += `<div class="box"><h2>${data[index].titre}</h2></div>`;
+        listeMorceau.innerHTML += `<div class="box"><h2>${data[index].titre}</h2><span>${data[index].duree}</span></div>`;
       }
     }
   }
 }
 
 /**
- * Fonction pour charger les musiques écoutées
+ * Fonction pour charger les musiques écoutées appelé par le bouton du menu
  */
 function loadListened() {
   document.getElementById('main').innerHTML = `
